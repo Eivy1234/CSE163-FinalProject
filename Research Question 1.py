d9deda83a861029b82e7f4a89df2d49988d801d9
eivy1234 <eivy1234@uw.edu>
2022-06-01 15:45:12 -0700

    Create Research Question 1.py

---
+++ Research Question 1.py
@@ -0,0 +1,5 @@
+# Research Question 1
+# This file contains the code for the visuals of the plots for the first research Question
+# The visuals will be done with plotly and use multiple music streaming datasets
+
+Print("helloWorld")
