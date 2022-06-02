ce21c204e3ec97ef79260c2b2656b875846273e4
eivy1234 <eivy1234@uw.edu>
2022-06-01 17:22:54 -0700

    added utills file

--- Research Question 1.py
+++ Research Question 1.py
@@ -2,4 +2,21 @@
 # This file contains the code for the visuals of the plots for the first research Question
 # The visuals will be done with plotly and use multiple music streaming datasets
 
-Print("helloWorld")
+
+def testing(number):
+    double = number * 2
+    print(double)
+    return double
+
+def repeatByNum(word, num):
+    for i in range(1, num + 1):
+        print(word)
+
+
+
+def main():
+    testing("hi")
+    repeatByNum("fuck", 3)
+
+if __name__ == '__main__':
+    main()
