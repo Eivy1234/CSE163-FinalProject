da226a87be0d4db25fdeb7523576b4955b13d750
eivy1234 <eivy1234@uw.edu>
2022-06-06 20:22:15 -0700

    added functions

--- Research Question 1.py
+++ Research Question 1.py
@@ -34,17 +34,65 @@ def loading_data(data1, data2):
     print(data2)
 
 def plot1(dataset):
-    a = dataset.head()
-    b = px.line(a, x='title', y='dur')
+    bounds_data = dataset[(dataset['year released'] >= 2009) & (dataset['year released'] <= 2019)]
+    data = bounds_data.groupby("artist").mean()
+    b = px.line(data, x='year released', y='dur')
+    b.show()
 
-def testing(number):
-    double = number * 2
-    print(double)
-    return double
 
-def repeatByNum(word, num):
-    for i in range(1, num + 1):
-        print(word)
+def top_fast_genre(df):
+    # top genre vs duration
+    # how does the duration of a song relate to the top genre in songs from 2010 to 2019?
+    data2 = df.groupby('top genre', as_index=False).mean()
+    data2 
+
+    fig1 = px.bar(data2, x='top genre', y='dur', labels={'x':'Genre', 'y':'Duration (sec)'})
+
+    fig1.update_layout(
+    title_text='Top Genre from 2010-2019 Mean Duration'
+    )
+
+    fig1.show()
+
+    fastest_genres = data2.nlargest(n=10, columns=['dur'])
+    fig2 = px.bar(fastest_genres, x='top genre', y='dur', labels={'x':'Genre', 'y':'Duration (sec)'})
+
+    fig2.update_layout(
+    title_text='Top Genre from 2010-2019 Mean Duration'
+    )
+
+    fig2.show()
+
+    test1 = data2.nlargest(n=10, columns=['dur'])
+    print(test1)
+
+
+def mean_dur_bpm_in_group_type(df):
+    avg_dur = df.groupby(['year released', 'artist type'], as_index=False)[('dur','bpm')].mean()
+    fig = px.line_polar(avg_dur, r = 'dur', theta ='artist type', color='bpm',symbol="year released", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+    fig.show()
+
+
+def popular_streamers(df):
+    # not printing
+    data3 = df.groupby('Artist', as_index=False)['Streams'].max()
+    g = data3.nlargest(n=15, columns=['Streams'])
+    fig = px.bar(g, x='Artist', y='Streams')
+    fig.update_layout(
+    title_text='Top Genre from 2010-2019 Mean Duration'
+    )
+
+    fig.show()
+
+
+def funnel_graph(df):
+    group_by_artist = df2.groupby(['Artist','Track Name'], sort=False, as_index=False)['Streams'].max()
+    # data4 = group_by_artist.groupby(['Track Name'] )['Streams'].max()
+    k = group_by_artist.nlargest(n=15, columns=['Streams'])
+    # l = k.groupby(['Artist', 'Track Name'])['Streams'].max().nlargest(n=15)
+    px.funnel(k, x='Track Name', y='Streams', color='Artist')
+
 
 def spotify_2010_2019_plot(df):
     """
@@ -54,7 +102,7 @@ def spotify_2010_2019_plot(df):
     genre can be easily noticible. This is done by taking the dataframe as a
     parameter.
     """
-    years_2010 = df['year released'] >= 2015
+    years_2010 = df['year released'] >= 2010
     years_2019 = df['year released'] <= 2019
     data = df[years_2010 & years_2019]
     top_genre = data.groupby('year released')['top genre'].max()
@@ -70,17 +118,18 @@ def spotify_2010_2019_plot(df):
     plt.show()
     
     
-#this is just a comment
 def main():
+    # read datasets => dataframe
     df1 = pd.read_csv(top_100, on_bad_lines='skip')
     df2 = pd.read_csv(data2, sep='#', header=None)
+
     # loading_data(df1, df2)
     # load_in_data(df1, df2)
-    plot1(df1)
+    #plot1(df1)
+    top_fast_genre(df1)
+    mean_dur_bpm_in_group_type(df1)
+    popular_streamers(df2)
     spotify_2010_2019_plot(df1)
-    # testing(69)
-    # repeatByNum("j", 5)
-    # repeatByNum("ahhh", 5)
 
 if __name__ == '__main__':
     main()
